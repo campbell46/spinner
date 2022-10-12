05174b3733ef4313c2208f2505ea5f1ff4ac1ae0
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-11 20:25:20 -0400

    added comments

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,6 @@
 const spinner = '|/-\\|/-\\|';
 
+//loop through spinner characters, setting an accumulating timer for each one
 for (let i = 0, j = 100; i < spinner.length; i++, j += 200) {
   setTimeout(() => {
     process.stdout.write(`\r${spinner[i]}   `);
